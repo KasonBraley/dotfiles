28b2ad222ad61c64538832a14821dc6cd7967d5f
Kason <kbraley9@gmail.com>
2021-06-28 08:59:16 -0700

    add neovim

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,140 @@
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local opt = {}
+
+-- dont copy any deleted text , this is disabled by default so uncomment the below mappings if you want them
+--[[ remove this line
+
+map("n", "dd", [=[ "_dd ]=], opt)
+map("v", "dd", [=[ "_dd ]=], opt)
+map("v", "x", [=[ "_x ]=], opt)
+
+ this line too ]]
+ --
+-- map navigation between splits using <C-{h,j,k,l}>
+map("n", "<C-k>", [[<Cmd> wincmd k<CR>]])
+map("n", "<C-l>", [[<Cmd> wincmd l<CR>]])
+map("n", "<C-h>", [[<Cmd> wincmd h<CR>]])
+map("n", "<C-j>", [[<Cmd> wincmd j<CR>]])
+
+-- horizontal & vertical resize
+map("n", "+", [[<Cmd> vertical resize +5<CR>]])
+map("n", "_", [[<Cmd> vertical resize -5<CR>]])
+map("n", "<leader>=", [[<Cmd> resize +5<CR>]])
+map("n", "<leader>-", [[<Cmd> resize -5<CR>]])
+
+-- OPEN TERMINALS --
+map("n", "<C-v>", [[<Cmd>vnew term:// <CR>]], opt) -- term over right
+map("n", "<C-x>", [[<Cmd> split term:// | resize 10 <CR>]], opt) --  term bottom
+map("n", "<C-t>t", [[<Cmd> tabnew | term <CR>]], opt) -- term newtab
+
+-- copy whole file content
+map("n", "<C-a>", [[ <Cmd> %y+<CR>]], opt)
+
+-- remove selection
+map("n", "<leader><Esc>", [[<Cmd> noh <CR>]], { silent = true })
+
+-- save
+map("n", "zz", [[<Cmd> w<CR>]], { silent = true })
+
+-- return normal mode on esc in terminal
+map("t", "<Esc>", [[<C-\><C-n>]], { silent = true })
+
+-- toggle numbers
+map("n", "<leader>n", [[ <Cmd> set nu!<CR>]], opt)
+
+ -- lazy git
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("Closing terminal")
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+
+-- Truezen.nvim
+map("n", "<leader>z", [[ <Cmd> TZAtaraxis<CR>]], opt)
+map("n", "<leader>m", [[ <Cmd> TZMinimalist<CR>]], opt)
+
+map("n", "<C-s>", [[ <Cmd> w <CR>]], opt)
+-- vim.cmd("inoremap jh <Esc>")
+
+-- Commenter Keybinding
+map("n", "<leader>/", ":CommentToggle<CR>", {noremap = true, silent = true})
+map("v", "<leader>/", ":CommentToggle<CR>", {noremap = true, silent = true})
+
+-- compe stuff
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col(".") - 1
+    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+        return true
+    else
+        return false
+    end
+end
+
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn["compe#complete"]()
+    end
+end
+
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+        return t "<Plug>(vsnip-jump-prev)"
+    else
+        return t "<S-Tab>"
+    end
+end
+
+function _G.completions()
+    local npairs = require("nvim-autopairs")
+    if vim.fn.pumvisible() == 1 then
+        if vim.fn.complete_info()["selected"] ~= -1 then
+            return vim.fn["compe#confirm"]("<CR>")
+        end
+    end
+    return npairs.check_break_line_char()
+end
+
+--  compe mappings
+map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map("i", "<CR>", "v:lua.completions()", {expr = true})
---
+++ nvim/lua/treesitter-nvim.lua
@@ -0,0 +1,21 @@
+local ts_config = require("nvim-treesitter.configs")
+
+ts_config.setup {
+    ensure_installed = {
+        "javascript",
+        "html",
+        "css",
+        "typescript",
+        "tsx",
+        "json",
+        "bash",
+        "lua",
+        "python",
+        -- "rust",
+        -- "go"
+    },
+    highlight = {
+        enable = true,
+        use_languagetree = true
+    }
+}
