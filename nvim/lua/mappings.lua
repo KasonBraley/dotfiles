da3d94c22e3ff74ba14ada9b8ef0fa897b9cfabd
Kason <kbraley9@gmail.com>
2021-06-29 22:04:40 -0700

    mapping adjustments

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,9 +1,9 @@
 local function map(mode, lhs, rhs, opts)
-    local options = {noremap = true}
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  local options = {noremap = true}
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 local opt = {}
@@ -16,7 +16,7 @@ map("v", "dd", [=[ "_dd ]=], opt)
 map("v", "x", [=[ "_x ]=], opt)
 
  this line too ]]
- --
+--
 -- map navigation between splits using <C-{h,j,k,l}>
 map("n", "<C-k>", [[<Cmd> wincmd k<CR>]])
 map("n", "<C-l>", [[<Cmd> wincmd l<CR>]])
@@ -38,43 +38,45 @@ map("n", "<C-t>t", [[<Cmd> tabnew | term <CR>]], opt) -- term newtab
 map("n", "<C-a>", [[ <Cmd> %y+<CR>]], opt)
 
 -- remove selection
-map("n", "<leader><Esc>", [[<Cmd> noh <CR>]], { silent = true })
+map("n", "<leader><Esc>", [[<Cmd> noh <CR>]], {silent = true})
 
 -- save
-map("n", "zz", [[<Cmd> w<CR>]], { silent = true })
+map("n", "zz", [[<Cmd> w<CR>]], {silent = true})
 
 -- return normal mode on esc in terminal
-map("t", "<Esc>", [[<C-\><C-n>]], { silent = true })
+map("t", "<Esc>", [[<C-\><C-n>]], {silent = true})
 
 -- toggle numbers
 map("n", "<leader>n", [[ <Cmd> set nu!<CR>]], opt)
 
- -- lazy git
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({
-  cmd = "lazygit",
-  dir = "git_dir",
-  direction = "float",
-  float_opts = {
-    border = "double",
-  },
-  -- function to run on opening the terminal
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
-  end,
-  -- function to run on closing the terminal
-  on_close = function(term)
-    vim.cmd("Closing terminal")
-  end,
-})
+-- lazy git
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit =
+  Terminal:new(
+  {
+    cmd = "lazygit",
+    dir = "git_dir",
+    direction = "float",
+    float_opts = {
+      border = "double"
+    },
+    -- function to run on opening the terminal
+    on_open = function(term)
+      vim.cmd("startinsert!")
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    end,
+    -- function to run on closing the terminal
+    on_close = function(term)
+      vim.cmd("Closing terminal")
+    end
+  }
+)
 
 function _lazygit_toggle()
   lazygit:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
-
 
 -- Truezen.nvim
 map("n", "<leader>z", [[ <Cmd> TZAtaraxis<CR>]], opt)
@@ -90,46 +92,46 @@ map("v", "<leader>/", ":CommentToggle<CR>", {noremap = true, silent = true})
 -- compe stuff
 
 local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 local check_back_space = function()
-    local col = vim.fn.col(".") - 1
-    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-        return true
-    else
-        return false
-    end
+  local col = vim.fn.col(".") - 1
+  if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+    return true
+  else
+    return false
+  end
 end
 
 _G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-n>"
-    elseif check_back_space() then
-        return t "<Tab>"
-    else
-        return vim.fn["compe#complete"]()
-    end
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn["compe#complete"]()
+  end
 end
 
 _G.s_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-p>"
-    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-        return t "<Plug>(vsnip-jump-prev)"
-    else
-        return t "<S-Tab>"
-    end
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
+  else
+    return t "<S-Tab>"
+  end
 end
 
 function _G.completions()
-    local npairs = require("nvim-autopairs")
-    if vim.fn.pumvisible() == 1 then
-        if vim.fn.complete_info()["selected"] ~= -1 then
-            return vim.fn["compe#confirm"]("<CR>")
-        end
+  local npairs = require("nvim-autopairs")
+  if vim.fn.pumvisible() == 1 then
+    if vim.fn.complete_info()["selected"] ~= -1 then
+      return vim.fn["compe#confirm"]("<CR>")
     end
-    return npairs.check_break_line_char()
+  end
+  return npairs.check_break_line_char()
 end
 
 --  compe mappings
@@ -138,3 +140,6 @@ map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 map("i", "<CR>", "v:lua.completions()", {expr = true})
+
+-- Markdown
+map("n", "<leader>p", [[<Cmd> Glow <CR>]])
