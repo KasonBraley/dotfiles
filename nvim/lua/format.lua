6d673afc8e49dad7e1926c26e423e894c758def5
Kason <kbraley9@gmail.com>
2021-06-29 22:05:17 -0700

    new file for new formatter plugin

---
+++ nvim/lua/format.lua
@@ -0,0 +1,43 @@
+require("formatter").setup(
+  {
+    logging = false,
+    filetype = {
+      javascript = {
+        -- prettier
+        function()
+          return {
+            exe = "prettierd",
+            args = {vim.api.nvim_buf_get_name(0)},
+            stdin = true
+          }
+        end
+      },
+      lua = {
+        -- luafmt
+        function()
+          return {
+            exe = "luafmt",
+            args = {"--indent-count", 2, "--stdin"},
+            stdin = true
+          }
+        end
+      }
+    }
+  }
+)
+
+vim.api.nvim_exec(
+  [[
+augroup FormatAutogroup
+  autocmd!
+  autocmd FileType javascript autocmd BufWritePost <buffer> FormatWrite
+  autocmd FileType lua autocmd BufWritePost <buffer> FormatWrite
+  autocmd FileType typescript autocmd BufWritePost <buffer> FormatWrite
+augroup end
+]],
+  true
+)
+
+local opt = {noremap = true, silent = true}
+
+vim.api.nvim_set_keymap("n", "<Leader>fm", [[<Cmd> Format<CR>]], opt)
