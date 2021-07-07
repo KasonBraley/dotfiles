6fe60bf4978151389a20c13b4a65b0d9c96c9cc9
Kason <kbraley9@gmail.com>
2021-07-07 13:28:49 -0700

    lazy loading, move mappings

--- nvim/lua/nvimTree.lua
+++ nvim/lua/nvimTree.lua
@@ -1,101 +1,63 @@
-local g = vim.g
+local M = {}
 
-vim.o.termguicolors = true
+M.config = function()
+  local g = vim.g
 
-g.nvim_tree_side = "right"
-g.nvim_tree_width = 30
-g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
-g.nvim_tree_auto_open = 1
-g.nvim_tree_auto_close = 1
-g.nvim_tree_auto_ignore_ft = {"dashboard"}
-g.nvim_tree_quit_on_open = 0
-g.nvim_tree_follow = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_root_folder_modifier = ":t"
-g.nvim_tree_tab_open = 1
-g.nvim_tree_allow_resize = 1
+  vim.o.termguicolors = true
 
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1
-}
+  g.nvim_tree_side = "right"
+  g.nvim_tree_width = 25
+  g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
+  g.nvim_tree_gitignore = 1
+  g.nvim_tree_auto_ignore_ft = {"dashboard"} -- don't open tree on specific fiypes.
+  g.nvim_tree_auto_open = 1
+  g.nvim_tree_auto_close = 1 -- closes tree when it's the last window
+  g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
+  g.nvim_tree_follow = 1
+  g.nvim_tree_indent_markers = 1
+  g.nvim_tree_hide_dotfiles = 1
+  g.nvim_tree_git_hl = 1
+  g.nvim_tree_highlight_opened_files = 0
+  g.nvim_tree_root_folder_modifier = ":t"
+  g.nvim_tree_tab_open = 1
+  g.nvim_tree_allow_resize = 1
+  g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
+  g.nvim_tree_disable_netrw = 1
+  g.nvim_tree_hijack_netrw = 0
+  g.nvim_tree_update_cwd = 1
 
-g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "✗",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    untracked = "★",
-    deleted = "",
-    ignored = "◌"
-  },
-  folder = {
-    default = "",
-    open = "",
-    symlink = "",
-    empty = "",
-    empty_open = "",
-    symlink_open = ""
-  },
-  lsp = {
-    hint = "",
-    info = "",
-    warning = "",
-    error = ""
+  g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1
+    -- folder_arrows= 1
   }
-}
-
--- Mappings for nvimtree
-
-vim.api.nvim_set_keymap(
-  "n",
-  "<C-n>",
-  ":NvimTreeToggle<CR>",
-  {
-    noremap = true,
-    silent = true
+  g.nvim_tree_icons = {
+    default = "",
+    symlink = "",
+    git = {
+      unstaged = "✗",
+      staged = "✓",
+      unmerged = "",
+      renamed = "➜",
+      untracked = "★",
+      deleted = "",
+      ignored = "◌"
+    },
+    folder = {
+      -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
+      -- arrow_open = "",
+      -- arrow_closed = "",
+      default = "",
+      open = "",
+      empty = "", -- 
+      empty_open = "",
+      symlink = "",
+      symlink_open = ""
+    }
   }
-)
 
-local tree_cb = require "nvim-tree.config".nvim_tree_callback
+  local tree_cb = require "nvim-tree.config".nvim_tree_callback
+end
 
-g.nvim_tree_bindings = {
-  ["u"] = ":lua require'some_module'.some_function()<cr>",
-  -- default mappings
-  ["<CR>"] = tree_cb("edit"),
-  ["o"] = tree_cb("edit"),
-  ["<2-LeftMouse>"] = tree_cb("edit"),
-  ["<2-RightMouse>"] = tree_cb("cd"),
-  ["<C-]>"] = tree_cb("cd"),
-  ["<C-v>"] = tree_cb("vsplit"),
-  ["<C-x>"] = tree_cb("split"),
-  ["<C-t>"] = tree_cb("tabnew"),
-  ["<"] = tree_cb("prev_sibling"),
-  [">"] = tree_cb("next_sibling"),
-  ["<BS>"] = tree_cb("close_node"),
-  ["<S-CR>"] = tree_cb("close_node"),
-  ["<Tab>"] = tree_cb("preview"),
-  ["I"] = tree_cb("toggle_ignored"),
-  ["H"] = tree_cb("toggle_dotfiles"),
-  ["R"] = tree_cb("refresh"),
-  ["a"] = tree_cb("create"),
-  ["d"] = tree_cb("remove"),
-  ["r"] = tree_cb("rename"),
-  ["<C-r>"] = tree_cb("full_rename"),
-  ["x"] = tree_cb("cut"),
-  ["c"] = tree_cb("copy"),
-  ["p"] = tree_cb("paste"),
-  ["y"] = tree_cb("copy_name"),
-  ["Y"] = tree_cb("copy_path"),
-  ["gy"] = tree_cb("copy_absolute_path"),
-  ["[g"] = tree_cb("prev_git_item"),
-  ["]g"] = tree_cb("next_git_item"),
-  ["-"] = tree_cb("dir_up"),
-  ["q"] = tree_cb("close")
-}
+return M
