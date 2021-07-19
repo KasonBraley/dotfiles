9873324dc9dbd93745e6faf28ae6f9444c8c54e5
kason <kbraley9@gmail.com>
2021-07-18 19:27:17 -0700

    stop using fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -51,12 +51,9 @@ M.config = function()
       buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
     },
     extensions = {
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = false, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-        -- the default case_mode is "smart_case"
+      fzy_native = {
+        override_generic_sorter = true,
+        override_file_sorter = true
       },
       media_files = {
         filetypes = {"png", "webp", "jpg", "jpeg"},
@@ -65,7 +62,7 @@ M.config = function()
     }
   }
 
-  require("telescope").load_extension("fzf")
+  require("telescope").load_extension("fzy_native")
   require("telescope").load_extension("media_files")
 end
 
