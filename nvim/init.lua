ec06ed86c023fd35b61ac4be51fb82b666360ab4
kason <kbraley9@gmail.com>
2021-07-23 11:16:46 -0700

    remove async use

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,10 @@
-require "options"
-
 local my_modules = {
+  "options",
   "pluginList",
   "mappings",
   "utils"
 }
 
-local async
-async =
-  vim.loop.new_async(
-  vim.schedule_wrap(
-    function()
-      for i = 1, #my_modules, 1 do
-        pcall(require, my_modules[i])
-      end
-      async:close()
-    end
-  )
-)
-async:send()
+for i = 1, #my_modules, 1 do
+  pcall(require, my_modules[i])
+end
