c12bcf517fce8eb150ac834df1c6dccee2e00a57
kason <kbraley9@gmail.com>
2021-08-10 13:20:08 -0700

    remove pcall and nvim-comment plugin

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -1,72 +1,51 @@
 local M = {}
 
 M.colorizer = function()
-  local present, colorizer = pcall(require, "colorizer")
-  if present then
-    colorizer.setup()
-    vim.cmd("ColorizerReloadAllBuffers")
-  end
-end
-
-M.comment = function()
-  local present, nvim_comment = pcall(require, "nvim_comment")
-  if present then
-    nvim_comment.setup()
-  end
+  require("colorizer").setup()
 end
 
 M.lspkind = function()
-  local present, lspkind = pcall(require, "lspkind")
-  if present then
-    lspkind.init()
-  end
+  require("lspkind").init()
 end
 
 M.neoscroll = function()
-  pcall(
-    function()
-      require("neoscroll").setup()
-    end
-  )
+  require("neoscroll").setup()
 end
 
 M.blankline = function()
   vim.g.indentLine_enabled = 1
   vim.g.indent_blankline_char = "▏"
 
-  vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-  vim.g.indent_blankline_buftype_exclude = {"terminal"}
+  vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard", "packer" }
+  vim.g.indent_blankline_buftype_exclude = { "terminal" }
 
   vim.g.indent_blankline_show_trailing_blankline_indent = false
   vim.g.indent_blankline_show_first_indent_level = false
 end
 
+-- custom telescope search functions
 M.search_dev = function()
-  require("telescope.builtin").find_files(
-    {
-      prompt_title = "~ dev ~ ",
-      shorten_path = false,
-      cwd = "~/dev/",
-      layout_strategy = "horizontal",
-      layout_config = {
-        preview_width = 0.65
-      }
-    }
-  )
+  require("telescope.builtin").find_files {
+    prompt_title = "~ dev ~ ",
+    shorten_path = false,
+    cwd = "~/dev/",
+    layout_strategy = "horizontal",
+    layout_config = {
+      preview_width = 0.65,
+    },
+  }
 end
 
 M.search_dotfiles = function()
-  require("telescope.builtin").find_files(
-    {
-      prompt_title = "~ dotfiles ~ ",
-      shorten_path = false,
-      cwd = "~/dotfiles/nvim",
-      layout_strategy = "horizontal",
-      layout_config = {
-        preview_width = 0.65
-      }
-    }
-  )
+  require("telescope.builtin").find_files {
+    prompt_title = "~ dotfiles ~ ",
+    shorten_path = false,
+    cwd = "~/dotfiles/nvim",
+    layout_strategy = "horizontal",
+    layout_config = {
+      preview_width = 0.65,
+    },
+  }
 end
 
 return M
