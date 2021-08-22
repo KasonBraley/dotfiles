70ef77df27d0deff1ca002158a5dcd791f177181
kason <kbraley9@gmail.com>
2021-08-22 16:41:32 -0700

    feat(nvim): Add plugins - lint, theme, git
    Add 3 plugins to neovim configuration and workflow.
    
    - nvim-lint
    - tokyonight theme
    - Neogit

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -8,6 +8,38 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, _, params, client_id, _)
+  local config = { -- your config
+    underline = true,
+    virtual_text = {
+      prefix = "■ ",
+      spacing = 4,
+    },
+    signs = true,
+    update_in_insert = true,
+  }
+  local uri = params.uri
+  local bufnr = vim.uri_to_bufnr(uri)
+
+  if not bufnr then
+    return
+  end
+
+  local diagnostics = params.diagnostics
+
+  for i, v in ipairs(diagnostics) do
+    diagnostics[i].message = string.format("%s: %s", v.source, v.message)
+  end
+
+  vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
+
+  if not vim.api.nvim_buf_is_loaded(bufnr) then
+    return
+  end
+
+  vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
+end
+
 -- lspInstall + lspconfig stuff
 
 local function setup_servers()
