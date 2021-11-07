559ab979c10cebb3cb0d975537e0d1a4ceaf4cf2
kason <kbraley9@gmail.com>
2021-11-07 12:41:03 -0700

    (nvim): Use prettier_d_slim; don't fmt on save

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -2,8 +2,8 @@ local format = require("formatter")
 
 local prettier = function()
   return {
-    exe = "prettierd",
-    args = { vim.api.nvim_buf_get_name(0) },
+    exe = "prettier_d_slim",
+    args = {"--stdin", "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
     stdin = true,
   }
 end
@@ -33,12 +33,12 @@ format.setup({
 })
 
 -- format on save
-vim.api.nvim_exec(
-  [[
-augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.html,*css,*json,*lua FormatWrite
-augroup END
-]],
-  true
-)
+-- vim.api.nvim_exec(
+--   [[
+-- augroup FormatAutogroup
+--   autocmd!
+--   autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.html,*css,*json,*lua FormatWrite
+-- augroup END
+-- ]],
+--   true
+-- )
