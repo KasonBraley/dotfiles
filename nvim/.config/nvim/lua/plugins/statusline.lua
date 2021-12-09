1aa145b0df9a0ff06ae922bf3c4d44e7579e2b59
kason <kbraley9@gmail.com>
2021-12-09 11:32:35 -0700

    (nvim):lualine: nvim-lualine/lualine.nvim@4231b631

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -14,7 +14,7 @@ lualine.setup({
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch" },
-    lualine_c = { "filename", { "diagnostics", sources = { "nvim_lsp" } }, "diff" },
+    lualine_c = { "filename", "diagnostics", "diff" },
     lualine_x = { "encoding", "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
