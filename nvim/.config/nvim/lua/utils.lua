194dc14d2cecad4925ca8110fb496805992a39f0
kason <kbraley9@gmail.com>
2021-12-24 21:50:29 -0700

    nvim: Add goimports function, run on save

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -7,6 +7,7 @@ vim.api.nvim_exec(
    au BufRead,BufNewFile *.md setlocal spell
    au BufWrite * set fileformat=unix 
    au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif
+   au BufWritePre *.go lua require('plugins.others').goimports(1000)
 ]],
   false
 )
