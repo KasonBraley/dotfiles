07db0bd2319473174cb14168c55e861fc67f659e
kason <kbraley9@gmail.com>
2022-01-29 23:18:59 -0700

    nvim: Update gitsigns for breaking change

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -33,5 +33,7 @@ gitsigns.setup({
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = false,
-  use_internal_diff = true, -- If luajit is present
+  diff_opts = {
+    internal = true, -- If luajit is present
+  },
 })
