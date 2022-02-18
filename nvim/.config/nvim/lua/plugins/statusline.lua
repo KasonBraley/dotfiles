2d480a5a0d59a066dd5744259905347909dd9cd3
kason <kbraley9@gmail.com>
2022-02-18 12:10:21 -0700

    nvim: Statusline use relative path for file name

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -14,7 +14,7 @@ lualine.setup({
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch" },
-    lualine_c = { "filename", "diagnostics", "diff" },
+    lualine_c = { { "filename", path = 1 }, "diagnostics", "diff" },
     lualine_x = { "encoding", "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
