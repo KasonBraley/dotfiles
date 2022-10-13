069e3949175f5e63dcd5f7764ebbc8842d52cf71
kason <kbraley9@gmail.com>
2022-10-13 13:50:28 -0700

    Update goimports func

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -318,28 +318,25 @@ require("formatter").setup({
   },
 })
 
-local goimports = function(wait_ms)
+local go_org_imports = function(wait_ms)
   local params = vim.lsp.util.make_range_params()
   params.context = { only = { "source.organizeImports" } }
   local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-  for _, res in pairs(result or {}) do
+  for cid, res in pairs(result or {}) do
     for _, r in pairs(res.result or {}) do
       if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
-      else
-        vim.lsp.buf.execute_command(r.command)
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
       end
     end
   end
-
-  vim.lsp.buf.formatting_sync()
 end
 
 -- format Go with goimports
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-    goimports(1000)
+    go_org_imports(1000)
   end,
 })
 
