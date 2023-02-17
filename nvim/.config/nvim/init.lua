2a6964b35413d515c77130e25a2bdb59d250825a
kason <kbraley9@gmail.com>
2023-02-17 11:41:19 -0700

    nvim: Add vim.opt.autoindent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -112,6 +112,7 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.inccommand = "split"
 vim.opt.equalalways = false
