b84c78e7b56b146696ea41cd217d85a5d40500b3
kason <kbraley9@gmail.com>
2023-02-17 11:35:17 -0700

    nvim: `sumneko_lua` -> `lua_ls`

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -723,13 +723,19 @@ lspconfig.gopls.setup({
     },
 })
 
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
     settings = {
         Lua = {
             workspace = {
                 checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+            format = {
+                enable = false,
             },
         },
     },
