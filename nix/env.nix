6da06e5e2d7951f3e85e5063c292b63f1f9b8d63
Kason Braley <kbraley9@gmail.com>
2023-11-05 16:48:02 -0700

    switching computers, push nix

---
+++ nix/env.nix
@@ -0,0 +1,37 @@
+with import <nixpkgs> {}; [
+  air
+  bat
+# GNU utils
+  coreutils-full
+  fd
+# GNU find utils
+  findutils
+  fzf
+  gdb
+  gh
+  git
+  git-extras
+# GNU sed
+  gnused
+  go
+  # godoc ??
+  # goimports
+  golangci-lint
+# GNU grep
+  grep
+  htop
+  jq
+  neovim
+  nodejs_20
+  pprof
+  # python
+  ripgrep
+  ssh-copy-id
+  stow
+  tree
+# benchstat
+# govulncheck install with just go install?
+# multi-gitter install with just go install?
+# pkgsite ??
+# zsh-syntax-highlighting
+]
