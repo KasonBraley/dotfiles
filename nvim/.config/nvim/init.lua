9589630836c3f4a3ba732f9c75cf02f3b3b4c9d6
Kason Braley <kbraley9@gmail.com>
2023-12-30 09:58:34 -0700

    nvim: Remove manual treesitter-templ install
    nvim-treesitter supports it direclty now.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -603,17 +603,6 @@ require("nvim-treesitter.configs").setup({
   }
 })
 
-local treesitter_parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-treesitter_parser_config.templ = {
-  install_info = {
-    url = "https://github.com/vrischmann/tree-sitter-templ.git",
-    files = { "src/parser.c", "src/scanner.c" },
-    branch = "master",
-  },
-}
-
-vim.treesitter.language.register('templ', 'templ')
-
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local function on_attach(_, bufnr)
