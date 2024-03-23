07971ffc64841eded8e721eeff5ae9cb0b0a8cc7
Kason Braley <kbraley9@gmail.com>
2024-03-23 15:45:59 -0700

    nix: update packages

--- nix/env.nix
+++ nix/env.nix
@@ -1,35 +1,35 @@
 with import <nixpkgs> {}; [
-  air
-  bat
-# GNU utils
-  coreutils-full
-  fd
-# GNU find utils
-  findutils
-  fzf
-  gdb
-  gh
-  git
-  git-extras
-# GNU sed
-  gnused
-  go
-  # godoc ??
-  # goimports
-  golangci-lint
-# GNU grep
-  grep
-  htop
-  jq
-  neovim
-  nodejs_20
-  pprof
-  # python
-  ripgrep
-  ssh-copy-id
-  stow
-  NIXPKGS_ALLOW_UNFREE=1 terraform
-  tree
+air-1.46.0
+awscli2-2.13.28
+bat-0.24.0
+cloc-1.98
+coreutils-9.3
+fd-8.7.1
+findutils-4.9.0
+fsouza-prettierd-0.25.1
+fzf-0.43.0
+gcc-wrapper-12.3.0
+gh-2.37.0
+git-2.42.0
+gnumake-4.4.1
+gnused-4.9
+golangci-lint-1.55.1
+htop-3.2.2
+inetutils-2.4
+jq-1.7
+k6-0.47.0
+neovim-0.9.5
+nodejs-20.9.0
+postgresql-13.13
+pprof-unstable-2023-07-05
+ripgrep-13.0.0
+scorecard-4.12.0
+ssh-copy-id-9.5p1
+stow-2.3.1
+terraform-1.6.2
+tree-2.1.1
+trivy-0.48.1
+zig-0.11.0
 # benchstat
 # govulncheck install with just go install?
 # multi-gitter install with just go install?
