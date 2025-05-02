777e22b3537dc8bfe028de70e167220df29c11e5
Kason Braley <kbraley9@gmail.com>
2025-05-02 11:47:56 -0700

    move lualine setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -740,7 +740,48 @@ require("lazy").setup({
   },
 
   -- Status Line
-  "nvim-lualine/lualine.nvim",
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      -- #373b41 greyish text fg
+      -- #81a2be light blue - default statusline color
+      local statusLineTheme = {
+        normal = {
+          a = { bg = "#f2f2f2", fg = "#000000" },
+          b = { bg = "#f2f2f2", fg = "#000000" },
+          c = { bg = "#f2f2f2", fg = "#000000" },
+          z = { bg = "#f2f2f2", fg = "#000000" },
+        },
+        insert = {
+          a = { bg = "#f2f2f2", fg = "#000000" },
+          b = { bg = "#f2f2f2", fg = "#000000" },
+          c = { bg = "#f2f2f2", fg = "#000000" },
+          z = { bg = "#f2f2f2", fg = "#000000" },
+        },
+      }
+
+      local opts = {
+        options = {
+          icons_enabled = false,
+          theme = statusLineTheme,
+          component_separators = { "", "" },
+          section_separators = { "", "" },
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = {},
+          lualine_b = { "branch", "diff" },
+          lualine_c = { "%=", { "filename", path = 1 } },
+          lualine_x = { "diagnostics" },
+          lualine_y = { "encoding", "filetype" },
+          lualine_z = { "location" },
+        },
+        extensions = { "quickfix" },
+      }
+
+      return opts
+    end,
+  },
 
   {
     "ThePrimeagen/harpoon",
@@ -942,43 +983,6 @@ vim.keymap.set({ "n", "v" }, "<Leader>fm", function()
     lsp_fallback = true,
   })
 end)
-
--- #373b41 greyish text fg
--- #81a2be light blue - default statusline color
-local statusLineTheme = {
-  normal = {
-    a = { bg = "#f2f2f2", fg = "#000000" },
-    b = { bg = "#f2f2f2", fg = "#000000" },
-    c = { bg = "#f2f2f2", fg = "#000000" },
-    z = { bg = "#f2f2f2", fg = "#000000" },
-  },
-  insert = {
-    a = { bg = "#f2f2f2", fg = "#000000" },
-    b = { bg = "#f2f2f2", fg = "#000000" },
-    c = { bg = "#f2f2f2", fg = "#000000" },
-    z = { bg = "#f2f2f2", fg = "#000000" },
-  },
-}
-
--- statusline
-require("lualine").setup({
-  options = {
-    icons_enabled = false,
-    theme = statusLineTheme,
-    component_separators = { "", "" },
-    section_separators = { "", "" },
-    globalstatus = true,
-  },
-  sections = {
-    lualine_a = {},
-    lualine_b = { "branch", "diff" },
-    lualine_c = { "%=", { "filename", path = 1 } },
-    lualine_x = { "diagnostics" },
-    lualine_y = { "encoding", "filetype" },
-    lualine_z = { "location" },
-  },
-  extensions = { "quickfix" },
-})
 
 vim.keymap.set("n", "<Leader>l", ":lua require('textcase').current_word('to_camel_case')<CR> <CR>")
 vim.keymap.set("n", "<Leader>L", ":lua require('textcase').current_word('to_pascal_case')<CR> <CR>")
